2118889b5ad5c801508018056fc06d477d5f9860
Karina <lyakhova_karina@list.ru>
2023-03-23 22:32:28 +0300

    10.1

--- ls 10.py
+++ ls 10.py
@@ -4,4 +4,4 @@ for i in range(n):
     v = int(input("введите 0, если мнетка лежит ввер орлом, 1, если решкой: "))
     if v == 1:
         k += 1
-print(k if k<n/2 else n-k)
+print(k if k < n / 2 else n-k)
